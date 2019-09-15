842663f1edea1f4abaf922458875f4f8c7ec4d62
François Falala-Sechet <francois@clevy.io>
2019-09-15 21:43:52 +0200

    tmp: commit dist

---
+++ dist/lib/streamToS3.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = async (response, S3Client, s3Options = {}, uploadOptions = {}) => {
+  const uploadParams = {
+    Body: response,
+    ...s3Options
+  }; // TODO: s3 options could be parameterized and taken from the options object
+
+  const options = {
+    partSize: 10 * 1024 * 1024,
+    queueSize: 1
+  };
+  const uploadProgress = typeof uploadOptions.uploadProgress === "function" ? uploadOptions.uploadProgress : () => {};
+  return await S3Client.upload(uploadParams, options).on("httpUploadProgress", uploadProgress).promise();
+};
